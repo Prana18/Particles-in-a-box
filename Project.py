70ba3e2f1ca03ca87172dc1a8acf18f4dbdae914
Prana18 <p.karthikesu@lancaster.ac.uk>
2023-03-18 08:36:44 +0000

    Adding real time simulation
    using the funcanimation module we were able to simulate the movement of the particles in real time

--- Project.py
+++ Project.py
@@ -28,11 +28,10 @@ class Particle:
 class Box:
     def __init__(
         self,
-        time_increment = 1E-5,
+        time_increment = 1e-5,
         number_of_particles = 10,
         width = 10,
         height = 10
-        
     ):
         self.time_increment = time_increment
         self.number_of_particles = number_of_particles
@@ -41,32 +40,58 @@ class Box:
         self.particles = [Particle(i) for i in range(self.number_of_particles)]
 
     def random_pos(self):
-        for i in range (self.number_of_particles):
-            self.particles[i].position = np.random.uniform(self.width, self.height, 2)
+        for i in range(self.number_of_particles):
+            self.particles[i].position[0] = np.random.uniform(-self.width / 2, self.width / 2)
+            self.particles[i].position[1] = np.random.uniform(-self.height / 2, self.height / 2)
             self.particles[i].velocity = 20 * np.random.uniform(-1, 1, 2)
 
     def update_pos(self):
         for i in range (self.number_of_particles):
-            self.particles[i].position += self.time_increment * self.particles[i].velocity
+            self.particles[i].position[0] += self.time_increment * self.particles[i].velocity[0]
+            self.particles[i].position[1] += self.time_increment * self.particles[i].velocity[1]
 
-    def plot(self,colour):
-        for i in range (self.number_of_particles):
-            plt.scatter(box.particles[i].position[0], box.particles[i].position[1], color=colour, alpha=box.particles[i].gradient)
-            plt.xlabel('time')
-            plt.ylabel('position')
+    #def wall_collisions(self):
+    #    if 
 
-box = Box(1, 10, 10, 10)
+
+
+initial_colour = "blue"
+box = Box(1e-3, 10, 10, 10)
 box.random_pos()
-box.plot("red")
 
-for i in range(500):
-    for particle in box.particles:
-        particle.gradient = particle.gradient - (particle.gradient * 0.05)
-        box.update_pos()
-        box.plot("blue")
+
+fig, axis = plt.subplots()
+
+x=[]
+y=[]
+for i in range(box.number_of_particles):
+    x.append(box.particles[i].position[0])
+    y.append(box.particles[i].position[1])
+
+scatter = axis.scatter(x, y)
+
+
+#plt.scatter(box.particles[i].position[0], box.particles[i].position[1], initial_colour, alpha=box.particles[i].gradient)
+plt.xlabel('time')
+plt.ylabel('position')
+
+def plot(frame):
+    box.update_pos()
+    positions=[]
+    for i in range (box.number_of_particles):
+        positions.append(box.particles[i].position)
+    scatter.set_offsets(positions)
+
+
+animation = FuncAnimation(fig, func=plot, interval=100)
+
+# for i in range(500):
+#     for particle in box.particles:
+#         particle.gradient = particle.gradient - (particle.gradient * 0.05)
+#         box.update_pos()
+#         box.plot("blue")
 
 plt.show()
-
 
 
 
