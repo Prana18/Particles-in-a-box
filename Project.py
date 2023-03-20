c0dabfd57876c3b8b15c5e5219498b04a2c4b20f
Prana18 <p.karthikesu@lancaster.ac.uk>
2023-03-20 18:06:48 +0000

    Coding boundary conditions
    Today we adjusted the code so that the particle collide with the wall and their new velocity is equal to the negative of the original velocity

--- Project.py
+++ Project.py
@@ -23,6 +23,16 @@ class Particle:
             self.mass = mass
             self.colour = colour
             self.gradient = gradient
+
+    def setVelocity(self, x, y):
+        self.velocity[0] = x
+        self.velocity[1] = y
+        pass
+
+    def setPosition(self, x, y):
+        self.position[0] = x
+        self.position[1] = y
+        pass
         
 
 class Box:
@@ -30,8 +40,8 @@ class Box:
         self,
         time_increment = 1e-5,
         number_of_particles = 10,
-        width = 10,
-        height = 10
+        width = 5,
+        height = 5
     ):
         self.time_increment = time_increment
         self.number_of_particles = number_of_particles
@@ -46,17 +56,30 @@ class Box:
             self.particles[i].velocity = 20 * np.random.uniform(-1, 1, 2)
 
     def update_pos(self):
-        for i in range (self.number_of_particles):
-            self.particles[i].position[0] += self.time_increment * self.particles[i].velocity[0]
-            self.particles[i].position[1] += self.time_increment * self.particles[i].velocity[1]
+        for particle in self.particles:
 
-    #def wall_collisions(self):
-    #    if 
+            new_x = particle.position[0] + (self.time_increment * particle.velocity[0])
+            new_y = particle.position[1] + (self.time_increment * particle.velocity[1])
+            
+            if new_x <= -self.width or new_x >= self.width:
+                particle.setVelocity(particle.velocity[0] * -1, particle.velocity[1])
+
+            if new_y <= -self.height or new_y >= self.height:
+                particle.setVelocity(particle.velocity[0], particle.velocity[1] * -1)
+
+            particle.setPosition(new_x, new_y)
+
+    def wall_collisions(self):
+        for particle in self.particles:
+            if particle.position[0] == -self.width or particle.position[0] == self.width:
+                particle.setVelocity(particle.velocity[0] * -1, particle.velocity[1])
+
+            if particle.position[1] == -self.height or particle.position[1] == self.height:
+                particle.setVelocity(particle.velocity[0], particle.velocity[1] * -1)
 
 
-
-initial_colour = "blue"
-box = Box(1e-3, 10, 10, 10)
+initial_colour = "red"
+box = Box(1e-3, 1000, 2, 2)
 box.random_pos()
 
 
@@ -68,12 +91,12 @@ for i in range(box.number_of_particles):
     x.append(box.particles[i].position[0])
     y.append(box.particles[i].position[1])
 
-scatter = axis.scatter(x, y)
+scatter = axis.scatter(x, y,s=20,c='indigo',marker= '.')
 
-
-#plt.scatter(box.particles[i].position[0], box.particles[i].position[1], initial_colour, alpha=box.particles[i].gradient)
-plt.xlabel('time')
-plt.ylabel('position')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.ylim(-2,2)
+plt.xlim(-2,2)
 
 def plot(frame):
     box.update_pos()
@@ -83,13 +106,7 @@ def plot(frame):
     scatter.set_offsets(positions)
 
 
-animation = FuncAnimation(fig, func=plot, interval=100)
-
-# for i in range(500):
-#     for particle in box.particles:
-#         particle.gradient = particle.gradient - (particle.gradient * 0.05)
-#         box.update_pos()
-#         box.plot("blue")
+animation = FuncAnimation(fig, func=plot, interval=10)
 
 plt.show()
 
